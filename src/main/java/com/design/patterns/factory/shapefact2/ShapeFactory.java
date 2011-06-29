8793f239626660e7a70cf06594bd824a275acf4e
the.james.burton <the.james.burton@localhost>
2011-06-29 15:01:54 +0100

    corrected hardcoded package name

--- src/main/java/com/design/patterns/factory/shapefact2/ShapeFactory.java
+++ src/main/java/com/design/patterns/factory/shapefact2/ShapeFactory.java
@@ -9,22 +9,23 @@ abstract class ShapeFactory {
     private static Map factories = new HashMap();
 
     public static void addFactory(String id, ShapeFactory f) {
-        factories.put(id, f);
+	factories.put(id, f);
     }
 
     // A Template Method:
     public static final Shape createShape(String id) {
-        if (!factories.containsKey(id)) {
-            try {
-                // Load dynamically
-                Class.forName("factory.shapefact2." + id);
-            } catch (ClassNotFoundException e) {
-                throw new RuntimeException("Bad shape creation: " + id);
-            }
-            // See if it was put in:
-            if (!factories.containsKey(id))
-                throw new RuntimeException("Bad shape creation: " + id);
-        }
-        return ((ShapeFactory) factories.get(id)).create();
+	if (!factories.containsKey(id)) {
+	    try {
+		// Load dynamically
+		Class.forName("com.design.patterns.factory.shapefact2." + id);
+	    } catch (ClassNotFoundException e) {
+		throw new RuntimeException("Bad shape creation: " + id);
+	    }
+	    // See if it was put in:
+	    if (!factories.containsKey(id)) {
+		throw new RuntimeException("Bad shape creation: " + id);
+	    }
+	}
+	return ((ShapeFactory) factories.get(id)).create();
     }
 }
