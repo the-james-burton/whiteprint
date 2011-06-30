febd36c05afb5987cb4502bf9469e9a7839642aa
jimsey <jimsey@localhost>
2011-06-30 10:47:54 +0100

    use inherited logger

--- src/main/java/com/design/patterns/templatemethod/MyApp.java
+++ src/main/java/com/design/patterns/templatemethod/MyApp.java
@@ -1,10 +1,6 @@
 package com.design.patterns.templatemethod;
 
-import java.util.logging.Logger;
-
 class MyApp extends ApplicationFramework {
-    /** Standard java logging */
-    private final Logger log = Logger.getLogger(this.getClass().getName());
 
     @Override
     void customize1() {
